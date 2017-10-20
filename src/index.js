05781f241da7d158faf8017c6ce5824377fb4ace
jonesyk28 <jonesyk28@gmail.com>
2017-10-20 10:38:14 +0100

    create store and connect to redux logger

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
 
 import App from './components/App';
+
+import store from './store';
 
 import 'bulma/css/bulma.css';
 import 'font-awesome/css/font-awesome.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+  document.getElementById('root')
+);
