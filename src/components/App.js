797407c107621ca62c742a2631a48957d0acbe46
jonesyk28 <jonesyk28@gmail.com>
2017-10-20 09:03:36 +0100

    implement basic file system configure webpack and run build

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class App extends React.Component {
+  render () {
+    return (
+      <section className='section'>
+        <div id='App' className='container'>
+          <h1 className='title is-1'>NC News</h1>
+          <span className='icon'>
+            <i className='fa fa-circle'></i>
+          </span>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/App';
+
+import 'bulma/css/bulma.css';
+import 'font-awesome/css/font-awesome.min.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
