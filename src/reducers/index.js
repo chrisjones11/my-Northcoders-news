30f3581eaeb69c32cd521ffbbc3e02dbf25bb102
jonesyk28 <jonesyk28@gmail.com>
2017-10-20 11:34:23 +0100

    create  articlesReducer, articles actions and create spec file to test newReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -8,4 +8,3 @@ const reducer = combineReducers({
 });
 
 export default reducer;
-
--- test/reducer.spec.js
+++ test/reducer.spec.js
@@ -1,29 +1,29 @@
 import {expect} from 'chai';
-import reducer, {getInitialState} from '../src/reducers';
+import testReducer from '../src/reducers/test';
 import * as actionCreators from '../src/actions';
 
 describe('reducer', () => {
   describe('default behaviour', () => {
     it('returns the passed previous state if an unrecognised action is passed', () => {
-      const prevState = getInitialState();
+      const prevState = false;
       const action = {type: 'whatever'};
-      const newState = reducer(prevState, action);
+      const newState = testReducer(prevState, action);
       expect(newState).to.equal(prevState);
     });
     it('uses the initial state if no previous state is passed', () => {
       const action = {type: 'whatever'};
-      const newState = reducer(undefined, action);
-      expect(newState).to.eql(getInitialState());
+      const newState = testReducer(undefined, action);
+      expect(newState).to.eql(false);
     });
   });
   describe('handles TOGGLE_TEST action', () => {
     it('flips the state.test boolean', () => {
       let newState;
       const action = actionCreators.toggleTest();
-      newState = reducer(undefined, action);
-      expect(newState.test).to.be.true;
-      newState = reducer(newState, action);
-      expect(newState.test).to.be.false;
+      newState = testReducer(undefined, action);
+      expect(newState).to.be.true;
+      newState = testReducer(newState, action);
+      expect(newState).to.be.false;
     });
   });
 });
