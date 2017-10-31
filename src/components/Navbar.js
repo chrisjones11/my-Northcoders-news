820d98e15ebb36905d2b21d3f3ff2b39c7cfe9ac
jonesyk28 <jonesyk28@gmail.com>
2017-10-31 00:19:21 +0000

    columns to Navbar with bulma

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,17 +5,24 @@ import {Link} from 'react-router-dom';
 class Navbar extends React.Component {
   render() {
     return (
-      <div >
-        <h1 className='title is-1'>NC News</h1>
-        <button> 
-          <span className="icon">
-            <Link to='/'><i className="fa fa-home"></i></Link>
+      <div id= 'nav' className = 'columns'>
+        <div id= 'nav' className='column'>
+          <h1 className='title is-1' >NC News</h1>
+        </div>
+        <div id= 'nav' className='column'>
+          <button  > 
+            <span id= 'nav' className="icon" >
+              <Link to='/'><i className="fa fa-home"></i></Link>
             
-          </span>
-        </button>
-        <Link to='/football'>Football|</Link>
-        <Link to='/cooking'>Cooking|</Link>
-        <Link to='/coding'>coding</Link>
+            </span>
+      
+          </button>   
+        </div>
+        <div className='column'>
+          <Link to='/football'>Football|</Link>
+          <Link to='/cooking'>Cooking|</Link>
+          <Link to='/coding'>coding</Link>
+        </div>
       </div>
     );
   }
